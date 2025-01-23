21339616b95ab7d0769d86462f72a4a7babc573d
shrutee pimpare <shruteeshantilal_pimpare@epam.com>
2025-01-23 14:11:34 +0530

    feat: add Sum function and integrate with App

--- concov_demo/src/App.js
+++ concov_demo/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import { sum } from "./components/Sum";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>React Codecov Demo</h1>
+            <p>Sum of 2 and 3 is: {sum(2, 3)}</p>
+        </div>
+    );
 }
 
 export default App;
